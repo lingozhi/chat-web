89c30919a20aecbbe751c3b1a6653c8f588f53dc
chen.lingzhi <chen.lingzhi@qkupower.com>
2023-03-23 18:19:50 +0800

    初始化项目

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./app.css";
+
+class App extends Component {
+  render() {
+    return <div className="app">第一</div>;
+  }
+}
+
+export default App;
